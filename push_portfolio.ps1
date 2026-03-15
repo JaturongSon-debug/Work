8506d23be6f7424948cdce8270b76b475fb995db
phanlop <phanlopratchapan@gmail.com>
2026-03-15 11:45:50 +0700

    Add professional portfolio website for Jaturong Songpakdee level-up presentation

---
+++ push_portfolio.ps1
@@ -0,0 +1,8 @@
+# Portfolio Push Script
+
+git init
+git remote add origin https://github.com/JaturongSon-debug/Work.git
+git add portfolio/
+git commit -m "Add modern one-page portfolio for Jaturong Songpakde"
+git branch -M main
+git push -u origin main
